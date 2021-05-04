54f59960b3327ca3737aeb77deacbdda22258fa9
rt <mathisbesnard3@gmail.com>
2021-05-04 17:16:56 +0200

    views created

--- models/iut_class.py
+++ models/iut_class.py
@@ -4,8 +4,7 @@ class IutItBrand(models.Model):
     _name = 'iut.class'
     
     name = fields.Char('Nom', required = True)
-    level = fields.Selection([('level1', 'Seconde'),('level2', 'Première'),('level3', 'Terminale')], 'level', default='level1')
-    teacher_ids = fields.Many2many('iut.prof')
+    level = fields.Selection([('level1', 'Seconde'),('level2', 'Première'),('level3', 'Terminale')], 'Niveau', default='level1')
+    teacher_ids = fields.Many2many('res.partner')
     student_ids = fields.Many2one('iut.student')
     student_nb = fields.Integer('Nombre d`élèves')
-    
--- models/res_partner.py
+++ models/res_partner.py
@@ -2,3 +2,4 @@ from odoo import fields, models, api
 
 class res_partner(models.Model):
     class_ids = fields.Many2many('iut.class')
+    inherit='res.partner'
